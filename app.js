45ee76b112d5532c1b77e71cb315a3ee39ac8bda
Kathleen Bongco <kathleen.bongco@gmail.com>
2024-01-08 09:55:36 -0500

    starting to create json file with pop culture holidays

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 const date = new Date();
-console.log(date);
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+
+function getCurrentDate(day, month, year) {
+  return ` ${month}/${day}/${year}`
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const todaysDate = getCurrentDate(day, month, year)
+  document.getElementById('current-date').textContent = todaysDate;
+  console.log(todaysDate)
+})
+
